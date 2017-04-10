a0ffa7b3f78e45f7f585e9840ea358cf54d28d27
Albert Choy <albert@choy.net>
2017-04-09 23:48:21 -0500

    new

--- backend/py-server/app.py
+++ backend/py-server/app.py
@@ -1,13 +1,13 @@
 #!flask/bin/python
-from flask import Flask, jsonify
+from flask import Flask, jsonify, make_response
 
 app = Flask(__name__)
 
 photos = [
-    { 'url': '1.jpeg', 'size': 36, 'title': 'Beachside AL' },
-    { 'url': '2.jpeg', 'size': 19, 'title': 'Epica, 2' },
-    { 'url': '3.jpeg', 'size': 41 },
-    { 'url': '4.jpeg', 'size': 41, 'title': 'City museum AL'}
+    { 'url': 'Norwegian1.jpeg', 'size': 790, 'title': 'Norwegian 1' },
+    { 'url': 'Norwegian2.jpeg', 'size': 785, 'title': 'Norwegian 2' },
+    { 'url': 'cool_balcony.jpg', 'size': 93, 'title': 'cool balcony' },
+    { 'url': 'doctor_who.jpg', 'size': 887, 'title': 'Doctor Who'}
 ]
 
 @app.route('/')
@@ -16,7 +16,14 @@ def index():
 
 @app.route('/photos/list.json')
 def get_photos():
-    return jsonify(photos)
+    resp = make_response(jsonify(photos), 200)
+    resp.headers['Access-Control-Allow-Origin'] = '*'
+    return resp
 
+@app.route('/photo/<path:filename>', methods=['GET'])
+def download(filename):
+    dirpath = os.path.join(current_app.root_path, 'photos')
+    return send_from_directory(directory=dirpath, filename=filename)
+    
 if __name__ == '__main__':
     app.run(host='0.0.0.0',debug=True)
